a36e3375e0c5bb8251940080c6fed7eb19a0a5c3
hectorrami <hramirez4@uh.edu>
2020-08-05 19:36:13 -0500

    Dynamic Routes and first blog :fire:

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Link from 'next/link';
+import Date from '../components/date';
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -10,14 +13,38 @@ export default function Home() {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>
-          Hi, I'm Hector. I'm a software engineer from Houston, TX. Welcome to
-          my blog!
+          Welcome to my blog, <br /> I'm a Junior Software Developer that
+          studied computer science at the{' '}
+          <a href="https://www.uh.edu/">University of Houston.</a>
+          <br />I made this blog to simply share some of the things I've learned
+          as a developer, and some of the mistakes I've made along the way.
         </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
 }
---
+++ posts/vim.md
@@ -0,0 +1,12 @@
+---
+title: 'Vim'
+date: '2020-08-05'
+---
+
+**What is Vim?**
+
+To put it simply, Vim is a text editor. But it's not just like any other text editor. Vim is highly configurable and effiecient text editor. If you have a Unix system, chances are that you have Vim installed. For example, if you are running MacOs, open your terminal and type the following command: `vim test.txt`
+
+**How to exit Vim**
+
+So you've opened vim and now you can't exit. It seems so complicated I know. When I first started using it, I could not figure it out either. And just because I couldn't exit, I didn't understand why anyone even used this. But the truth is that vim is amazing once you get used to it. I actually used it to write the code of this entire blog.
